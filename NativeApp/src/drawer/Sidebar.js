1bfcf8e4bdd51e2e863934efee5d493019c30c1c
Dominika Papánová <dominika.papanova2@gmail.com>
2024-02-09 13:44:16 +0100

    new logo + changed sidebar

--- NativeApp/src/drawer/Sidebar.js
+++ NativeApp/src/drawer/Sidebar.js
@@ -1,7 +1,18 @@
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
 import Home from "../screens/Home";
 import VictoryScreen from "../screens/VictoryScreen";
 import CustomHeader from "./CustomHeader";
@@ -16,6 +27,9 @@ const HomeIcon = ({ focused, color, size }) => (
 const SettingsIcon = ({ focused, color, size }) => (
   <Ionicons name="settings-outline" size={size} color={color} />
 );
+const LogoutIcon = ({ focused, color, size }) => (
+  <Ionicons name="log-out-outline" size={size} color={color} />
+);
 
 const Sidebar = () => {
   return (
@@ -23,24 +37,91 @@ const Sidebar = () => {
       <Drawer.Navigator
         screenOptions={({ navigation, route }) => ({
           header: () => <CustomHeader navigation={navigation} route={route} />,
-          drawerStyle: { width: "45%" },
+          drawerStyle: {
+            width: "50%",
+          },
+          drawerInactiveTintColor: "black",
+          drawerActiveTintColor: "black",
         })}
+        drawerContent={(props) => <CustomDrawerContent {...props} />}
       >
         <Drawer.Screen
           key="home"
-          name="Screen1"
+          name="Home"
           component={Home}
-          options={{ drawerIcon: HomeIcon }}
+          options={{
+            drawerIcon: HomeIcon,
+            drawerItemStyle: styles.logoutDrawerItem,
+          }}
+          navigation={dis}
         />
         <Drawer.Screen
-          key="victory"
-          name="Screen2"
+          key="settings"
+          name="Settings"
           component={VictoryScreen}
-          options={{ drawerIcon: SettingsIcon }}
+          options={{
+            drawerIcon: SettingsIcon,
+            drawerItemStyle: styles.logoutDrawerItem,
+          }}
         />
       </Drawer.Navigator>
     </NavigationContainer>
   );
 };
+
+function CustomDrawerContent(props) {
+  return (
+    <SafeAreaView
+      style={{ flex: 1 }}
+      forceInset={{ top: "always", horizontal: "never" }}
+    >
+      <DrawerContentScrollView {...props}>
+        <DrawerItemList {...props} />
+      </DrawerContentScrollView>
+      <View>
+        <DrawerItem
+          label="Logout"
+          icon={LogoutIcon}
+          inactiveTintColor="black"
+          activeTintColor="black"
+          style={styles.logoutDrawerItem}
+          onPress={() => console.log("Logout pressed!")}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  drawerItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  drawerItemText: {
+    marginLeft: 20,
+    fontSize: 16,
+    color: "black",
+  },
+  logoutButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start", // Align the logout button to the start of the container
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginTop: "auto",
+  },
+  logoutDrawerItem: {
+    borderRadius: 0,
+    marginTop: "auto",
+    color: "black",
+    borderWidth: 1,
+    borderColor: "black",
+  },
+});
 
 export default Sidebar;
